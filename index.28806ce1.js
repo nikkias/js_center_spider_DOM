d4777a59ec98d48e9f058fb2e74e4fb1481dd894
newqou <137334905+newqou@users.noreply.github.com>
2023-12-02 18:18:26 +0200

    make build

---
+++ index.28806ce1.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top="".concat((e.clientHeight-t.offsetHeight)/2,"px"),t.style.left="".concat((e.clientWidth-t.offsetWidth)/2,"px");
+//# sourceMappingURL=index.28806ce1.js.map
